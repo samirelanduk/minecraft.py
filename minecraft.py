8c8aef5671b6224a31a8841ee380da0099bd327f
Sam Ireland <mail@samireland.com>
2023-01-25 21:55:54 +0000

    Shell function

--- minecraft.py
+++ minecraft.py
@@ -2,6 +2,15 @@ import os
 import subprocess
 from pathlib import Path
 
+def run(command):
+    """Runs a shell command."""
+
+    return subprocess.call(
+        command, shell=True, stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+
+
 def get_save_location():
     """Gets the location of the save files."""
 
@@ -16,13 +25,10 @@ def get_save_location():
 
 def check_save_is_git(name):
     """Checks if a save has had `git init` run on it."""
+
     save_loc = get_save_location() / name
     os.chdir(save_loc)
-    code = subprocess.call(
-        "git status", shell=True,
-        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
-    )
-    return code == 0
+    return run("git status") == 0
 
 
 def init_save(name):
@@ -31,7 +37,4 @@ def init_save(name):
     if check_save_is_git(name): return
     save_loc = get_save_location() / name
     os.chdir(save_loc)
-    subprocess.call(
-        "git init", shell=True,
-        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
-    )
+    run("git init")
